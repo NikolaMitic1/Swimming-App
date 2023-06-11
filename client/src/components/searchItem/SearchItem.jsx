e0cc5880a0e69003a8827ae9f42c35e25501e84e
Nikola Mitic <nmitic304@gmail.com>
2023-06-11 20:17:21 +0200

    Swimming-App Day 15

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './searchItem.scss'
 
-const SearchItem = () => {
+const SearchItem = (item) => {
   return (
     <div className='searchItem'>
         <img src="../../../public/1200px-Baku_Aquatic_Palace,_Olympic_Pool.jpg" alt="" className="siImg" />
         <div className="siDesc">
-            <h1>Beogradska Arena</h1>
-            <span className='siDistance'>500m from center</span>
+            <h1>item.name</h1>
+            <span className='siDistance'>{item.gender} {item.distance} {item.style}</span>
             <span className='siTaxiOp'>Free airport taxi</span>
-            <span className="siSubtitle">Swimming pool with Air coditioning</span>
+            <span className="siSubtitle">{item.location}</span>
             <span className="siFatures">1 Swimming pool •  10 bathroom</span>
             <span className="siPrijava">Free prijava</span>
             <span className='siCancel'>Mozete odustati u bilo koje vreme</span>
